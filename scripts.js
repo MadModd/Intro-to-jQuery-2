f454c8c0782374fadf8a48e5cb5250c28ed641b0
MadModd <66334384+MadModd@users.noreply.github.com>
2020-06-06 16:30:21 +0200

    halfsol

---
+++ scripts.js
@@ -0,0 +1,11 @@
+let color = "purple";
+let number = "10";
+let word = "cool",
+if (color.equals("purple")) {
+    $('.box1').css('background', 'purple');
+};
+if (number > 100) {
+    $('.box2').text("whoah, that's a big number"),
+    } else {
+    $('.box2').text("just a regular number, please");
+};
